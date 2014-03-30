21e8b675e6bc761209096800a0dbf6779ba7c815
Varad Pathak <pathak.varad@gmail.com>
2014-03-29 18:04:22 -0700

    get links of all the articles

---
+++ Scrapper/article.py
@@ -0,0 +1,14 @@
+'''
+Created on Mar 29, 2014
+
+@author: vspathak
+'''
+
+class article(object):
+    def __init__(self, title, date, text, url, ID):
+        self.Title = title
+        self.Date = date
+        self.Text = text
+        self.URL = url
+        self.id = ID
+        
