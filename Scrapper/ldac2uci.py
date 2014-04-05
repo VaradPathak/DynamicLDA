1997eda518796d19c79de60f1abcf85bc099d65d
pearsauce <atma.persaud@gmail.com>
2014-04-04 18:06:44 -0700

    fixed ldac2uci so that we increment the wordids

--- Scrapper/ldac2uci.py
+++ Scrapper/ldac2uci.py
@@ -8,7 +8,7 @@ for i in range(len(doclines)):
     line = doclines[i].strip().split(' ')[1:]
     for elt in line:
         pieces = elt.split(':')
-        outfile.write(str(i+1) + ' ' + pieces[0] + ' ' + pieces[1] + '\n')
+        outfile.write(str(i+1) + ' ' + pieces[0]+1 + ' ' + pieces[1] + '\n')
     
 outfile.close()
 
