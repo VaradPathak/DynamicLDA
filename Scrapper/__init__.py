256092f320f855e0aa80151c65e99dc7e5fe3ff5
Varad Pathak <pathak.varad@gmail.com>
2014-04-09 20:32:24 -0700

    complete stemming and scrapping

--- Scrapper/__init__.py
+++ Scrapper/__init__.py
@@ -1,22 +1,33 @@
 from lxml import html
 import requests
 import article as ac
+import sys
+import random
 import nltk
 import re
 from stemming.porter2 import stem
 
 
 docId = 0  
-#f = open('2014.txt', 'w')
-seqfile = open('seqfile.txt', 'w')
-for yr in range(2013, 2014):
+if len(sys.argv) >= 4:
+    theyear = int(sys.argv[1])
+    firstmonth = int(sys.argv[2])
+    num_months = int(sys.argv[3])
+    seqfile = open('seq-' + str(theyear) + '-' + str(firstmonth) + '-' + str(num_months) + '.txt', 'w')
+
+else:
+    print 'usage: python __init__.py year firstmonth num_months'
+    sys.exit(0)
+
+for yr in range(theyear, theyear + 1):
     year = 'http://www.reuters.com/resources/archive/us/' + str(yr)
-    for mnth in range(3, 13):
+    for mnth in range(firstmonth, firstmonth + num_months):
         if(mnth < 10):
             month = '0' + str(mnth)
         else:
             month = str(mnth)
         
+        monthdocs = 0
         for day in range(1, 32):
             if(day < 10):
                 URL = year + month + '0' + str(day) + '.html'
@@ -25,20 +36,18 @@ for yr in range(2013, 2014):
             
             page = requests.get(URL)
             tree = html.fromstring(page.text)
-        # This will create a list of article URLs:
             URLs = tree.xpath('//div[@class="headlineMed"]/a/@href')
-        #     Title = tree.xpath('//div[@class="headlineMed"]/a/text()')
             date = URL[-13:-5]
-            #seqfile.write(str(len(URLs)))
-            daydocs = 0
-            for num in range(0, len(URLs)): #len(URLs)
-                docId = docId + 1
-                doc = ac.article('', date, '', URLs[num], docId)
+            f = open('output/' + str(date) + '.txt', 'w')
+            # generate the random vector(python generate a sample without 
+            # replacement from a range of numbers)
+             
+            for num in random.sample(range(0, len(URLs)), int(len(URLs) / 10)):
+                doc = ac.article('', date, '', URLs[num], -1)
                 curpage = requests.get(doc.URL)
                 curtree = html.fromstring(curpage.text)
                 Title = curtree.xpath('//*[@id="content"]/div[4]/div/div[3]/div[1]/h1/text()')
                 Paragraphs = curtree.xpath('//*[@id="articleText"]/p/text()')
-                # Location = tree.xpath('//*[@id="articleInfo"]/p[2]/span[1]/text()')
                 if len(Title) > 0:
                     doc.Title = Title[0].replace('\"', '')
                     Paragraphs.append(Title[0])
@@ -46,21 +55,27 @@ for yr in range(2013, 2014):
                 doc.Text = doc.Text.replace('\n', ' ')
                 doc.Text = doc.Text.replace('\"', '')
 
-                if(doc.Text and not doc.Title==doc.Text):
+                if(len(doc.Text.split()) > 100):
+                    docId = docId + 1
+                    doc.id = docId
                     print doc.id
-                    daydocs = daydocs + 1
-                    f = open('output/'+str(doc.Date)+'_'+str(doc.id)+'.txt', 'w')
-                    #f.write(str(doc.id) + "," + str(doc.Date) + ',\"' + doc.Title.encode('utf-8') + '\",\"' + doc.Text.encode('utf-8') + '\"\n')
+                    monthdocs = monthdocs + 1
                     
-                    raw = re.sub('[^A-Za-z]+', ' ', doc.Text)
-                    raw = raw.lower()
-                    tokens = raw.split()
+                    docText = re.sub('[^A-Za-z]+', ' ', doc.Text)
+                    docTitle = re.sub('[^A-Za-z]+', ' ', doc.Title)
+                    docText = docTitle + ', ' + docText  
+                    docText = docText.lower()
+                    tokens = docText.split()
 
-                    raw = " ".join([stem(t) for t in tokens])
+                    docText = " ".join([stem(t) for t in tokens])
 
-                    f.write(doc.Title.encode('utf-8') + ', ' + raw.encode('utf-8') + '\n')
-                    f.close()
-            seqfile.write(str(daydocs)+'\n')
+                    rawTitle = re.sub('[^A-Za-z]+', ' ', doc.Title)
+                    rawTitle = rawTitle.lower()
+                    tokens = rawTitle.split()
+                    rawTitle = " ".join([stem(t) for t in tokens])
+
+                    f.write(docText.encode('utf-8') + '\n')
+
+            f.close()
+        seqfile.write(str(theyear) + '-' + str(mnth) + ':' + str(monthdocs) + '\n')
 seqfile.close()
-#f.close()
-    
--- Scrapper/getarticle.py
+++ Scrapper/getarticle.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from lxml import html
 import requests
-#http://www.reuters.com/article/2012/12/02/us-space-france-russia-idUSBRE8B101L20121202
-#http://www.reuters.com/article/2007/01/02/music-jazz-chicago-dc-idUSN2927338620070102
-#http://www.reuters.com/article/2014/03/28/us-microsoft-office-ipad-idUSBREA2Q1MV20140328
+# http://www.reuters.com/article/2012/12/02/us-space-france-russia-idUSBRE8B101L20121202
+# http://www.reuters.com/article/2007/01/02/music-jazz-chicago-dc-idUSN2927338620070102
+# http://www.reuters.com/article/2014/03/28/us-microsoft-office-ipad-idUSBREA2Q1MV20140328
 page = requests.get('http://www.reuters.com/article/2014/01/02/walmart-china-idUSL3N0KC0LH20140102')
 tree = html.fromstring(page.text)
 
 # This will create a list of article URLs:
-#URL = tree.xpath('//div[@class="headlineMed"]/a/@href')
-#Title = tree.xpath('//div[@class="headlineMed"]/a/text()'
+# URL = tree.xpath('//div[@class="headlineMed"]/a/@href')
+# Title = tree.xpath('//div[@class="headlineMed"]/a/text()'
 Title = tree.xpath('//*[@id="content"]/div[4]/div/div[3]/div[1]/h1/text()')
 
 Location = tree.xpath('//*[@id="articleInfo"]/p[2]/span[1]/text()')
