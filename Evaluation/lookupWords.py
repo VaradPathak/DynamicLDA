b708812a8aa38a2734fc6b29a307b0ddf659d87e
Varad Pathak <pathak.varad@gmail.com>
2014-04-26 22:53:50 -0700

    Fixed Errors

--- Evaluation/lookupWords.py
+++ Evaluation/lookupWords.py
@@ -3,9 +3,11 @@ import sys
 
 
 def buildtriple(idprob,vocab):
-    (word_id, prob) = idprob.split(':')
-    word = vocab[int(word_id)]
-    return (word, word_id, prob)
+#     if (len(idprob) == 0):
+#         print 'working'
+    word_Prob = idprob.split(':')
+    word = vocab[int(word_Prob[0])]
+    return (word, word_Prob[0], word_Prob[1])
     
 #input: directory of input files, directory for output files, vocab file
 if len(sys.argv) >= 3:
@@ -15,11 +17,19 @@ if len(sys.argv) >= 3:
 
     vocabfile = open(vocabfilename, 'r')
     vocab = [word.strip() for word in vocabfile.readlines()]
+    vocabfile.close()
     
     #iterate through files of infolder
     for filename in os.listdir(infolder):
-        thefile = open(infolder + '/' + filename)
-        for topic in thefile.readlines():
-            outline = [buildtriple(idprob,vocab) for idprob in topic.split(',')]
-            map(lambda x: thefile.write(str(x)), outline)
-            thefile.write('\n')
+        infile = open(infolder + '/' + filename)
+        outfile = open(outfolder + '/' + filename, 'w')
+        for topic in infile.readlines():
+#             print topic
+            if(topic == ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n'):
+                print 'working'
+                continue
+            outline = [buildtriple(idprob,vocab) for idprob in topic[:-2].split(',')]
+            map(lambda x: outfile.write(str(x)), outline)
+            outfile.write('\n')
+        outfile.close()
+        infile.close()
